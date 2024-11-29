0738ade14d57295df511d0fc30f110612029bc2e
Miguel Luiz <122481365+miguelloq@users.noreply.github.com>
2024-11-29 15:21:20 -0300

    feat: adding users DI

---
+++ src/main/kotlin/com/example/modules/users/UserModule.kt
@@ -0,0 +1,20 @@
+package com.example.modules.users
+
+import com.example.modules.users.data.repository.UserRepositoryFake
+import com.example.modules.users.data.service.CreateTokenServiceImpl
+import com.example.modules.users.domain.repository.UserRepository
+import com.example.modules.users.domain.service.CreateTokenService
+import com.example.modules.users.domain.usecase.GetAllUserUsecase
+import com.example.modules.users.domain.usecase.LoginUserUsecase
+import com.example.modules.users.domain.usecase.RegisterUserUsecase
+import org.koin.core.KoinApplication
+import org.koin.dsl.module
+
+fun KoinApplication.userKoinModule() = module{
+    //single<UserRepository>{ PostgresUserRepository() }
+    single<UserRepository>{ UserRepositoryFake() }
+    single<CreateTokenService>{ CreateTokenServiceImpl() }
+    single{ RegisterUserUsecase(get()) }
+    single{ GetAllUserUsecase(get()) }
+    single{ LoginUserUsecase(get(),get()) }
+}
