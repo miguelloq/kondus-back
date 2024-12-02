4600019c1fe9bb5398779ce0d2714140eb576c1d
Miguel Luiz <122481365+miguelloq@users.noreply.github.com>
2024-12-02 15:45:02 -0300

    feat: local references user and users_houses table created

--- src/main/resources/db/migration/V2__create_locals_houses_table.sql
+++ src/main/resources/db/migration/V2__create_locals_houses_table.sql
@@ -5,7 +5,9 @@ create table locals(
     postal char(8) not null,
     name varchar(255) not null,
     description varchar(255) not null,
-    type varchar(255) not null
+    type varchar(255) not null,
+    user_id serial not null,
+    foreign key (user_id) references users(id) on delete cascade
 );
 
 create table houses(
@@ -13,4 +15,12 @@ create table houses(
     local_id serial not null,
     description varchar(255) not null,
     foreign key (local_id) references locals(id) on delete cascade
+);
+
+create table users_houses(
+    user_id serial not null,
+    house_id serial not null,
+    primary key (user_id,house_id),
+    foreign key (user_id) references users(id) on delete cascade,
+    foreign key (house_id) references houses(id) on delete cascade
 );
