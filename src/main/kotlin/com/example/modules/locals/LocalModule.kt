744e1cb8a18eaeaf5c067c617e9fde09367ef090
Miguel Luiz <122481365+miguelloq@users.noreply.github.com>
2024-12-06 17:42:22 -0300

    feat: adding houseRepository and others repos to localModule

--- src/main/kotlin/com/example/modules/locals/LocalModule.kt
+++ src/main/kotlin/com/example/modules/locals/LocalModule.kt
@@ -1,12 +1,21 @@
 package com.example.modules.locals
 
+import com.example.modules.locals.data.repository.HouseRepositoryImpl
 import com.example.modules.locals.data.repository.LocalRepositoryImpl
+import com.example.modules.locals.domain.repository.HouseRepository
 import com.example.modules.locals.domain.repository.LocalRepository
+import com.example.modules.locals.domain.usecase.AssociateHouseToUserUsecase
+import com.example.modules.locals.domain.usecase.CreateHouseUsecase
 import com.example.modules.locals.domain.usecase.CreateLocalUsecase
+import com.example.modules.locals.domain.usecase.GetAllHousesFromUserUsecase
 import org.koin.core.KoinApplication
 import org.koin.dsl.module
 
 fun KoinApplication.localKoinModule() = module{
     single<LocalRepository>{ LocalRepositoryImpl() }
+    single<HouseRepository>{ HouseRepositoryImpl(get()) }
     single{ CreateLocalUsecase(get()) }
+    single{ CreateHouseUsecase(get(),get()) }
+    single{ AssociateHouseToUserUsecase(get(), get()) }
+    single{ GetAllHousesFromUserUsecase(get()) }
 }
