d22a3be3162a5b348b7e449370b939330f8dab4b
Miguel Luiz <122481365+miguelloq@users.noreply.github.com>
2025-03-23 20:08:17 -0300

    feat: create table items and categories

---
+++ src/main/resources/db/migration/V4__create_items_table.sql
@@ -0,0 +1,25 @@
+CREATE TABLE items (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    type VARCHAR(20) CHECK (type IN ('product', 'service')) NOT NULL,
+    price NUMERIC(10,2),
+    unit VARCHAR(50), -- Exemplo: "hora", "unidade", "peça"
+    quantity INTEGER CHECK (quantity >= 0),
+    available BOOLEAN,
+    user_id SERIAL NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE categories (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) UNIQUE NOT NULL
+);
+
+CREATE TABLE item_categories (
+    item_id SERIAL NOT NULL,
+    category_id SERIAL NOT NULL,
+    PRIMARY KEY (item_id, category_id),
+    FOREIGN KEY (item_id) REFERENCES items(id) ON DELETE CASCADE,
+    FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE CASCADE
+);
