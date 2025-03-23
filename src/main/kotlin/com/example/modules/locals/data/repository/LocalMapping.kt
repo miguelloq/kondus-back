6180a5cb9a7c1c265c630b3b8cd5bf6e9318a8c8
Miguel Luiz <122481365+miguelloq@users.noreply.github.com>
2025-03-23 20:06:21 -0300

    fix: removing userId from LocalMapping

--- src/main/kotlin/com/example/modules/locals/data/repository/LocalMapping.kt
+++ src/main/kotlin/com/example/modules/locals/data/repository/LocalMapping.kt
@@ -1,8 +1,6 @@
 package com.example.modules.locals.data.repository
 
-import com.example.core.data.repository.CoreUserTable
 import org.jetbrains.exposed.dao.id.IntIdTable
-import org.jetbrains.exposed.sql.ReferenceOption
 
 object Locals : IntIdTable("locals") {
     val street = varchar("street", 255)
@@ -11,5 +9,4 @@ object Locals : IntIdTable("locals") {
     val name = varchar("name", 255)
     val description = varchar("description", 255)
     val type = varchar("type", 255)
-    val userId = integer("user_id").references(CoreUserTable.userId, onDelete = ReferenceOption.CASCADE)
 }
